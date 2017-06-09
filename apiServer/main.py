95ab3329efa64891050133c7297e7591b922754b
Javier Constain <constainopia@gmail.com>
2017-06-09 17:12:25 -0500

    Cleared the apiServer main test function

--- apiServer/main.py
+++ apiServer/main.py
@@ -9,24 +9,7 @@ HOST = environ.get('HOST', '127.0.0.1')
 PORT = int(environ.get('PORT', '4889'))
 DEBUG = True
 
-def main():
-    topic = topic_init()
-
-    image_list = [
-        'https://images.unsplash.com/photo-1428940253195-53483a1de2e6?dpr=1&auto=format&fit=crop&w=1500&h=1033&q=80&cs=tinysrgb&crop=&bg=',
-        'https://images.unsplash.com/photo-1476297820623-03984cf5cdbb?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=',
-        'https://images.unsplash.com/photo-1470163395405-d2b80e7450ed?dpr=1&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=',
-
-    ]
-    for image in image_list:
-        message = {'url': image}
-        data = json.dumps(message).encode('utf-8')
-        message_id = topic.publish(data)
-
-        print('Message {m_id} published.'.format(m_id=message_id))
-
 
 if __name__ == '__main__':
     print('Started')
-    # main()
     app.run(host=HOST, debug=DEBUG, port=PORT)
